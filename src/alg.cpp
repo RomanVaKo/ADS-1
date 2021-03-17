e8e197c72778165af9c92a50812e17d95ad32bf1
RomanVaKo <78842633+RomanVaKo@users.noreply.github.com>
2021-03-17 22:21:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  
+  int count;
+  int l = 0, r = size - 1;
+  int mid;
+  
+  while (l <= r) {
+    min = (r+l) / 2;
+    if (arr + mid > value) 
+      r = mid - 1;
+    else if (arr + mid < value)
+      l = mid + 1;
+    else 
+      count++;
+  }
+  
+  if (count > 0) 
+    return count;
+  else 
+    return 0; 
 }
