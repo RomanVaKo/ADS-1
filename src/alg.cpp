3817a0dac731ce570f794e025cdcf4f5b8d6fe3e
RomanVaKo <78842633+RomanVaKo@users.noreply.github.com>
2021-03-23 11:45:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,5 @@
+// Copyright 2021 NNTU-CS
+
 int cbinsearch(int* arr, int size, int value) {
     int count = 0;
     int l = 0, r = size - 1;
@@ -11,8 +13,8 @@ int cbinsearch(int* arr, int size, int value) {
             l = mid + 1;
         else break;
     }
-
-    if (arr[l] == value) {
+    
+    if (arr[mid] == value) {
         while (arr[l] == value) {
             count++;
             l++;
